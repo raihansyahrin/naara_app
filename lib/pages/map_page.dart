1bf725658699922d345480b0fd49d8c8314e3d8b
Muhammad Darrel Prawira <darrelprw@student.telkomuniversity.ac.id>
2024-01-12 16:47:10 +0700

    google maps setup

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -8,10 +9,32 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  late GoogleMapController mapController;
+
+  final LatLng _bandung = const LatLng(-6.914744, 107.609810);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("map nih")),
+    return MaterialApp(
+      home: Scaffold(
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _bandung,
+            zoom: 11.0,
+          ),
+          markers: {
+            const Marker(
+              markerId: const MarkerId("Tegallega Park"),
+              position: LatLng(-6.937170155175109, 107.60462976064048),
+            ),
+          },
+        ),
+      ),
     );
   }
 }
